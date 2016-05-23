30538dec3a10022e1808b51d22e5616db07b6310
Yufi <yfhj1990@hotmail.com>
2016-05-23 23:11:04 +0800

    update.

---
+++ webpack.config.babel.js
@@ -0,0 +1,18 @@
+import path from 'path'
+
+export default {
+    entry: './main.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
