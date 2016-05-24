19a25822d7861dc79e794d5e310e7036370f1a1c
Yufi <yfhj1990@hotmail.com>
2016-05-25 00:22:05 +0800

    update.

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 
+class Counter extends Component {
+    constructor() {
+        super()
+        this.state = { num: 0 }
+    }
+    incNum() {
+        this.setState({ num: this.state.num + 1 })
+    }
+    decNum() {
+        this.setState({ num: this.state.num - 1 })
+    }
+    render() {
+        return (
+          <div>
+            <span onClick={this.decNum.bind(this)}>-</span>
+            <span>{this.state.num}</span>
+            <span onClick={this.incNum.bind(this)}>+</span>
+          </div>
+        )
+    }
+}
+
+
 class App extends Component {
     loginfo(user) {
         console.log(user.name, user.editor)
@@ -14,6 +37,7 @@ class App extends Component {
                     n => <div onClick={() => this.loginfo(n) }>{`${n.name} - ${n.editor}`}</div>
                 )
             }
+            <Counter />
             </div>
         )
     }
